902c609349a200088cbb9d614ef5d3fa76c557e2
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-03-25 13:23:08 +0300

    -2147483648 to 2147483647 for signed short int

---
+++ data.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - function to test different data types
+ *Return: Return an integer
+ */
+int main(void)
+{
+	int a = -2147483647;
+
+	printf("%d \n", a);
+	printf("%u \n", a);
+	return (0);
+}
