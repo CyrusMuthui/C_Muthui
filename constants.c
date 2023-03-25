88af11819605ed9bc238c51e6a9c006182b5510c
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-03-25 12:54:57 +0300

    Constants in C

---
+++ constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+const int a = 10;
+#define A 30
+
+/**
+*main - Main function to declare constants
+*Return: Return int
+*/
+int main(void)
+{
+	printf("%d \n", a);
+	printf("%d \n", A);
+	return (0);
+}
