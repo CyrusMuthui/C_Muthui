00c003fd27a8e3bd56f4ae45aaed0d85e54082ef
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-05-10 09:42:48 +0300

    Null pointer

---
+++ null_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - null pointer practice
+ *Return: integer
+ */
+int main(void)
+{
+	int a = 5;
+	int *ptr = NULL;
+
+	printf("The null pointer equals:%p.\n", ptr);
+	return (0);
+}
