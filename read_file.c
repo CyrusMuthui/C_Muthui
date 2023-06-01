6a4bb62a29607efcb9a5e5ae73f5f6834feffa90
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-06-01 06:05:08 +0300

    C program to read a file

---
+++ read_file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *Return: integer
+ */
+int main(void)
+{
+	FILE *fp = NULL;
+	char ch;
+
+	fp = fopen("cyrus.txt", "r");
+	if (fp == NULL)
+	{
+		printf("File not found.\n");
+		exit(1);
+	}
+
+	while (!feof(fp))
+	{
+		ch = fgetc(fp);
+		printf("%c", ch);
+	}
+	return (0);
+}
