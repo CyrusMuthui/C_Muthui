f914431f54dfb985b361a9f5ae0d3d38cd153a8c
CyrusMuthui <cyrus.muthui20@gmail.com>
2023-06-09 05:59:33 +0300

    Changes to this but still with an error

--- calculator_using_enum_swtich.c
+++ calculator_using_enum_swtich.c
@@ -13,5 +13,26 @@ int main(void)
 		mul,
 		div
 	};
-	enum sign 
+	int a, b;
+	printf("Enter two numbers:\n");
+	scanf("%d %d", &a, &b);
+	enum sign c;
+	printf("Enter the operation you want:add, mul, sub or div:\n");
+	scanf("%d", (int *)c);
+	switch (c)
+	{
+	case add:
+		printf("Sum is %d.\n", a + b);
+		break;
+	case sub:
+		printf("Difference is %d.\n", a - b);
+		break;
+	case mul:
+		printf("Product is %d.\n", a * b);
+		break;
+	case div:
+		printf("Division is %d.\n", a / b);
+		break;
+	}
+	return (0);
 }
